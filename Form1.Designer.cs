17077c75067666641089bb91a05ca5fe3dbf2798
Nafissa <loadjianaffissatou@gmail.com>
2025-04-03 14:26:52 +0200

    Ajout du projet

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Pendu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // Création des contrôles
+            lblTitle = new System.Windows.Forms.Label();
+            lblWord = new System.Windows.Forms.Label();
+            lblAttempts = new System.Windows.Forms.Label();
+            txtLetter = new System.Windows.Forms.TextBox();
+            btnGuess = new System.Windows.Forms.Button();
+            lblResult = new System.Windows.Forms.Label();
+            lblVies = new System.Windows.Forms.Label();
+            lblMots = new System.Windows.Forms.Label();
+            btnRejouer = new System.Windows.Forms.Button();
+            btnQuitter = new System.Windows.Forms.Button();
+            picHangman = new System.Windows.Forms.PictureBox();
+            btnBack = new System.Windows.Forms.Button();
+
+            // Configuration des contrôles
+            ((System.ComponentModel.ISupportInitialize)picHangman).BeginInit();
+            SuspendLayout();
+
+            // lblTitle
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new System.Drawing.Font("Arial", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblTitle.Location = new System.Drawing.Point(120, 80);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new System.Drawing.Size(260, 45);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "WIN OR DIE";
+            lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // lblWord
+            lblWord.AutoSize = true;
+            lblWord.Font = new System.Drawing.Font("Arial", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblWord.Location = new System.Drawing.Point(120, 280);
+            lblWord.Name = "lblWord";
+            lblWord.Size = new System.Drawing.Size(260, 32);
+            lblWord.TabIndex = 1;
+            lblWord.Text = "_ _ _ _ _ _ _";
+            lblWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            // lblVies
+            lblVies.AutoSize = true;
+            lblVies.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblVies.Location = new System.Drawing.Point(360, 50);
+            lblVies.Name = "lblVies";
+            lblVies.Size = new System.Drawing.Size(100, 22);
+            lblVies.TabIndex = 2;
+            lblVies.Text = "Vie : 3 ❤❤❤";
+
+            // lblAttempts
+            lblAttempts.AutoSize = true;
+            lblAttempts.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblAttempts.Location = new System.Drawing.Point(70, 330);
+            lblAttempts.Name = "lblAttempts";
+            lblAttempts.Size = new System.Drawing.Size(260, 18);
+            lblAttempts.TabIndex = 3;
+            lblAttempts.Text = "Lettre utilisée : ";
+
+            // txtLetter
+            txtLetter.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            txtLetter.Location = new System.Drawing.Point(120, 370);
+            txtLetter.MaxLength = 1;
+            txtLetter.Name = "txtLetter";
+            txtLetter.Size = new System.Drawing.Size(50, 32);
+            txtLetter.TabIndex = 4;
+            txtLetter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            txtLetter.KeyPress += new System.Windows.Forms.KeyPressEventHandler(txtLetter_KeyPress);
+
+            // btnGuess
+            btnGuess.BackColor = System.Drawing.Color.FromArgb(82, 113, 255);
+            btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnGuess.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnGuess.ForeColor = System.Drawing.Color.White;
+            btnGuess.Location = new System.Drawing.Point(190, 370);
+            btnGuess.Name = "btnGuess";
+            btnGuess.Size = new System.Drawing.Size(120, 40);
+            btnGuess.TabIndex = 5;
+            btnGuess.Text = "Jouer";
+            btnGuess.UseVisualStyleBackColor = false;
+            btnGuess.Click += new System.EventHandler(btnGuess_Click);
+
+            // picHangman
+            picHangman.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            picHangman.Location = new System.Drawing.Point(140, 80);
+            picHangman.Name = "picHangman";
+            picHangman.Size = new System.Drawing.Size(180, 180);
+            picHangman.TabIndex = 6;
+            picHangman.TabStop = false;
+
+            // lblResult
+            lblResult.AutoSize = true;
+            lblResult.Font = new System.Drawing.Font("Arial", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            lblResult.Location = new System.Drawing.Point(120, 50);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new System.Drawing.Size(260, 35);
+            lblResult.TabIndex = 7;
+            lblResult.Text = "Tu as perdu";
+            lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            lblResult.Visible = false;
+
+            // lblMots
+            lblMots.AutoSize = true;
+            lblMots.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            lblMots.Location = new System.Drawing.Point(120, 120);
+            lblMots.Name = "lblMots";
+            lblMots.Size = new System.Drawing.Size(260, 25);
+            lblMots.TabIndex = 8;
+            lblMots.Text = "Mots : BONJOUR";
+            lblMots.Visible = false;
+
+            // btnRejouer
+            btnRejouer.BackColor = System.Drawing.Color.FromArgb(82, 113, 255);
+            btnRejouer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnRejouer.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnRejouer.ForeColor = System.Drawing.Color.White;
+            btnRejouer.Location = new System.Drawing.Point(140, 200);
+            btnRejouer.Name = "btnRejouer";
+            btnRejouer.Size = new System.Drawing.Size(180, 40);
+            btnRejouer.TabIndex = 9;
+            btnRejouer.Text = "Rejouer";
+            btnRejouer.UseVisualStyleBackColor = false;
+            btnRejouer.Visible = false;
+            btnRejouer.Click += new System.EventHandler(btnRejouer_Click);
+
+            // btnQuitter
+            btnQuitter.BackColor = System.Drawing.Color.FromArgb(82, 113, 255);
+            btnQuitter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnQuitter.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnQuitter.ForeColor = System.Drawing.Color.White;
+            btnQuitter.Location = new System.Drawing.Point(140, 260);
+            btnQuitter.Name = "btnQuitter";
+            btnQuitter.Size = new System.Drawing.Size(180, 40);
+            btnQuitter.TabIndex = 10;
+            btnQuitter.Text = "Quitter";
+            btnQuitter.UseVisualStyleBackColor = false;
+            btnQuitter.Visible = false;
+            btnQuitter.Click += new System.EventHandler(btnQuitter_Click);
+
+            // btnBack
+            btnBack.BackColor = System.Drawing.Color.FromArgb(82, 113, 255);
+            btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnBack.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnBack.ForeColor = System.Drawing.Color.White;
+            btnBack.Location = new System.Drawing.Point(20, 20);
+            btnBack.Name = "btnBack";
+            btnBack.Size = new System.Drawing.Size(50, 50);
+            btnBack.TabIndex = 11;
+            btnBack.Text = "←";
+            btnBack.UseVisualStyleBackColor = false;
+            btnBack.Visible = false;
+            btnBack.Click += new System.EventHandler(btnBack_Click);
+
+            // Form1
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.White;
+            ClientSize = new System.Drawing.Size(450, 600);
+            Controls.Add(btnBack);
+            Controls.Add(btnQuitter);
+            Controls.Add(btnRejouer);
+            Controls.Add(lblMots);
+            Controls.Add(lblResult);
+            Controls.Add(picHangman);
+            Controls.Add(btnGuess);
+            Controls.Add(txtLetter);
+            Controls.Add(lblAttempts);
+            Controls.Add(lblVies);
+            Controls.Add(lblWord);
+            Controls.Add(lblTitle);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Jeu du Pendu";
+            ((System.ComponentModel.ISupportInitialize)picHangman).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblWord;
+        private System.Windows.Forms.Label lblVies;
+        private System.Windows.Forms.Label lblAttempts;
+        private System.Windows.Forms.TextBox txtLetter;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.PictureBox picHangman;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblMots;
+        private System.Windows.Forms.Button btnRejouer;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,418 @@
+﻿// Form1.cs
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace Pendu
+{
+    public partial class Form1 : Form
+    {
+        private string wordToGuess;
+        private char[] guessedLetters;
+        private List<char> usedLetters = new List<char>();
+        private int maxGuesses = 6;
+        private int incorrectGuesses = 0;
+        private int lives = 3;
+
+
+        private Color primaryColor = Color.FromArgb(82, 113, 255);  // Bleu principal pour les boutons
+        private Color backgroundColor = Color.White;                // Fond blanc
+        private Color accentColor = Color.FromArgb(231, 76, 60);    // Rouge pour les erreurs
+        private Color successColor = Color.FromArgb(46, 204, 113);  // Vert pour les succès
+
+        public Form1()
+        {
+            InitializeComponent();
+            StyleApplication();
+            InitializeGame();
+        }
+
+        private void StyleApplication()
+        {
+            
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.BackColor = backgroundColor;
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            // Style pour les boutons
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderSize = 0;
+                    button.BackColor = primaryColor;
+                    button.ForeColor = Color.White;
+                    button.Font = new Font("Arial", 12F, FontStyle.Bold);
+                    button.Cursor = Cursors.Hand;
+                }
+            }
+
+            // Style pour l'écran titre
+            lblTitle.Font = new Font("Arial", 28F, FontStyle.Bold);
+            lblTitle.ForeColor = Color.Black;
+
+            // Style pour le mot à deviner
+            lblWord.Font = new Font("Arial", 20F, FontStyle.Bold);
+
+            // Style pour les champs d'information
+            lblVies.Font = new Font("Arial", 14F, FontStyle.Regular);
+            lblAttempts.Font = new Font("Arial", 12F, FontStyle.Regular);
+
+            // Style pour les écrans de fin
+            lblResult.Font = new Font("Arial", 22F, FontStyle.Bold);
+            lblMots.Font = new Font("Arial", 16F, FontStyle.Regular);
+
+            // Style pour la zone de texte
+            txtLetter.Font = new Font("Arial", 16F, FontStyle.Bold);
+            txtLetter.TextAlign = HorizontalAlignment.Center;
+            txtLetter.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void InitializeGame()
+        {
+            // Afficher l'écran de démarrage 
+            lblTitle.Visible = true;
+            lblTitle.Text = "WIN OR DIE";
+            lblTitle.ForeColor = Color.Black;
+
+            btnGuess.Text = "Jouer";
+            btnGuess.Visible = true;
+            btnGuess.Location = new Point((this.ClientSize.Width - btnGuess.Width) / 2, this.ClientSize.Height - 120);
+
+            // Cacher les éléments de jeu 
+            lblWord.Visible = false;
+            lblVies.Visible = false;
+            lblAttempts.Visible = false;
+            txtLetter.Visible = false;
+            picHangman.Visible = false;
+            btnBack.Visible = false;
+
+            // Cacher l'écran de fin 
+            lblResult.Visible = false;
+            lblMots.Visible = false;
+            btnRejouer.Visible = false;
+            btnQuitter.Visible = false;
+
+            // Réinitialiser les variables
+            usedLetters.Clear();
+            incorrectGuesses = 0;
+            lives = 3;
+        }
+
+        private void StartNewGame()
+        {
+            // Cacher l'écran de démarrage
+            lblTitle.Visible = false;
+
+            // Afficher l'écran de jeu 
+            lblWord.Visible = true;
+            lblVies.Visible = true;
+            lblAttempts.Visible = true;
+            txtLetter.Visible = true;
+            picHangman.Visible = true;
+            btnBack.Visible = true;
+
+            btnGuess.Location = new Point(txtLetter.Right + 10, txtLetter.Top - 5);
+            btnGuess.Text = "Valider";
+
+            // Réinitialiser les variables
+            usedLetters.Clear();
+            incorrectGuesses = 0;
+            UpdateLives();
+
+            // Charger un nouveau mot
+            LoadWordAsync();
+
+            // Dessiner le pendu initial
+            DrawHangman();
+        }
+
+        private async void LoadWordAsync()
+        {
+            wordToGuess = await GetRandomFrenchWord();
+            guessedLetters = new char[wordToGuess.Length];
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                guessedLetters[i] = '\0';
+            }
+            UpdateDisplay();
+        }
+
+        private async Task<string> GetRandomFrenchWord()
+        {
+            // Liste de mots au cas où l'API échoue
+            string[] frenchWords = { "BONJOUR", "MAISON", "VOITURE", "CHIEN", "CHAT", "ARBRE", "ETOILE", "MUSIQUE", "SOLEIL", "ORDINATEUR" };
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer CLE_API");
+                    HttpResponseMessage response = await client.GetAsync("https://www.blagues-api.fr/api/random");
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseContent = await response.Content.ReadAsStringAsync();
+                        JObject json = JObject.Parse(responseContent);
+                        string joke = json["joke"]?.ToString();
+                        string[] words = joke.Split(' ');
+
+                        // Filtrer les mots pour ne garder que ceux d'au moins 4 lettres
+                        var validWords = words.Where(w => w.Length >= 4 && !w.Contains("-") && !w.Contains("'")).ToArray();
+                        if (validWords.Length > 0)
+                        {
+                            return validWords[new Random().Next(validWords.Length)].ToUpper();
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    // En cas d'erreur, utiliser un mot de secours
+                }
+                return frenchWords[new Random().Next(frenchWords.Length)];
+            }
+        }
+
+        private void UpdateDisplay()
+        {
+            // Afficher le mot avec des tirets pour les lettres non devinées
+            lblWord.Text = string.Join(" ", guessedLetters.Select(c => c == '\0' ? '_' : c).ToArray());
+
+            // Afficher les lettres utilisées
+            if (usedLetters.Count > 0)
+            {
+                lblAttempts.Text = $"Lettres utilisées : {string.Join(", ", usedLetters)}";
+            }
+            else
+            {
+                lblAttempts.Text = "Lettres utilisées : ";
+            }
+
+            // Effacer la lettre dans la zone de texte
+            txtLetter.Text = "";
+            txtLetter.Focus();
+        }
+
+        private void UpdateLives()
+        {
+            string hearts = new string('❤', lives);
+            lblVies.Text = $"Vie : {lives} {hearts}";
+
+            // Mise à jour de la couleur en fonction du nombre de vies
+            if (lives == 1)
+                lblVies.ForeColor = accentColor;
+            else if (lives == 3)
+                lblVies.ForeColor = successColor;
+            else
+                lblVies.ForeColor = Color.Black;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // Si on est sur l'écran de démarrage
+            if (lblTitle.Visible && lblTitle.Text == "WIN OR DIE")
+            {
+                StartNewGame();
+                return;
+            }
+
+            // Vérification de la lettre
+            if (!string.IsNullOrWhiteSpace(txtLetter.Text))
+            {
+                char letter = char.ToUpper(txtLetter.Text[0]);
+
+                // Vérifier si c'est une lettre
+                if (!char.IsLetter(letter))
+                {
+                    MessageBox.Show("Veuillez entrer une lettre!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtLetter.Text = "";
+                    return;
+                }
+
+                // Vérifier si la lettre a déjà été utilisée
+                if (usedLetters.Contains(letter))
+                {
+                    MessageBox.Show("Vous avez déjà utilisé cette lettre!", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtLetter.Text = "";
+                    return;
+                }
+
+                // Ajouter la lettre aux lettres utilisées
+                usedLetters.Add(letter);
+
+                bool letterFound = false;
+                for (int i = 0; i < wordToGuess.Length; i++)
+                {
+                    if (wordToGuess[i] == letter)
+                    {
+                        guessedLetters[i] = letter;
+                        letterFound = true;
+                    }
+                }
+
+                if (!letterFound)
+                {
+                    incorrectGuesses++;
+                    DrawHangman();
+
+                    if (incorrectGuesses >= maxGuesses)
+                    {
+                        lives--;
+                        UpdateLives();
+
+                        if (lives <= 0)
+                        {
+                            GameOver(false);
+                        }
+                        else
+                        {
+                            // Réinitialiser le jeu pour un nouveau mot mais garder les vies
+                            usedLetters.Clear();
+                            incorrectGuesses = 0;
+                            LoadWordAsync();
+                            DrawHangman();
+                        }
+                    }
+                }
+
+                UpdateDisplay();
+                CheckGameStatus();
+            }
+        }
+
+        private void DrawHangman()
+        {
+            Bitmap bmp = new Bitmap(picHangman.Width, picHangman.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.Clear(Color.White);
+
+                // Dessiner le pendu en fonction du nombre d'erreurs
+                using (Pen pen = new Pen(Color.Black, 3))
+                {
+                    
+                    g.DrawLine(pen, 10, picHangman.Height - 10, picHangman.Width - 10, picHangman.Height - 10); // Base horizontale
+                    g.DrawLine(pen, 30, 10, 30, picHangman.Height - 10);   // Poteau vertical
+                    g.DrawLine(pen, 30, 10, picHangman.Width - 30, 10);    // Traverse horizontale
+                    g.DrawLine(pen, picHangman.Width - 30, 10, picHangman.Width - 30, 30);    // Corde
+
+                    if (incorrectGuesses >= 1)
+                    {
+                        // Tête
+                        g.DrawEllipse(pen, picHangman.Width - 45, 30, 30, 30);
+                    }
+                    if (incorrectGuesses >= 2)
+                    {
+                        // Corps
+                        g.DrawLine(pen, picHangman.Width - 30, 60, picHangman.Width - 30, 110);
+                    }
+                    if (incorrectGuesses >= 3)
+                    {
+                        // Bras gauche
+                        g.DrawLine(pen, picHangman.Width - 30, 70, picHangman.Width - 50, 90);
+                    }
+                    if (incorrectGuesses >= 4)
+                    {
+                        // Bras droit
+                        g.DrawLine(pen, picHangman.Width - 30, 70, picHangman.Width - 10, 90);
+                    }
+                    if (incorrectGuesses >= 5)
+                    {
+                        // Jambe gauche
+                        g.DrawLine(pen, picHangman.Width - 30, 110, picHangman.Width - 50, 140);
+                    }
+                    if (incorrectGuesses >= 6)
+                    {
+                        // Jambe droite
+                        g.DrawLine(pen, picHangman.Width - 30, 110, picHangman.Width - 10, 140);
+
+                        // Visage triste
+                        g.DrawArc(pen, picHangman.Width - 40, 42, 20, 10, 0, 180);
+                    }
+                }
+            }
+            picHangman.Image = bmp;
+        }
+
+        private void CheckGameStatus()
+        {
+            if (!guessedLetters.Contains('\0'))
+            {
+                GameOver(true);
+            }
+        }
+
+        private void GameOver(bool won)
+        {
+            // Cacher l'écran de jeu
+            lblWord.Visible = false;
+            lblVies.Visible = false;
+            lblAttempts.Visible = false;
+            txtLetter.Visible = false;
+            btnGuess.Visible = false;
+            picHangman.Visible = false;
+            btnBack.Visible = false;
+
+            // Afficher l'écran de fin 
+            lblResult.Visible = true;
+            lblMots.Visible = true;
+            btnRejouer.Visible = true;
+            btnQuitter.Visible = true;
+
+            // Centrer les contrôles
+            lblResult.Location = new Point((this.ClientSize.Width - lblResult.Width) / 2, 50);
+            lblMots.Location = new Point((this.ClientSize.Width - lblMots.Width) / 2, 120);
+            btnRejouer.Location = new Point((this.ClientSize.Width - btnRejouer.Width) / 2, 200);
+            btnQuitter.Location = new Point((this.ClientSize.Width - btnQuitter.Width) / 2, 260);
+
+            if (won)
+            {
+                lblResult.Text = "Bravo !!!";
+                lblResult.ForeColor = successColor;
+            }
+            else
+            {
+                lblResult.Text = "Tu as perdu";
+                lblResult.ForeColor = accentColor;
+            }
+
+            lblMots.Text = $"Mots : {wordToGuess}";
+        }
+
+        private void btnRejouer_Click(object sender, EventArgs e)
+        {
+            lives = 3;
+            InitializeGame();
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Retourner à l'écran d'accueil
+            InitializeGame();
+        }
+
+        private void txtLetter_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Permettre de valider avec la touche Entrée
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                btnGuess_Click(sender, e);
+            }
+        }
+    }
+}
